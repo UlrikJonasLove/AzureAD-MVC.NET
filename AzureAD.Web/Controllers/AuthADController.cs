28a2724291e985f19c8beba91743f21d6de11fee
UlrikJonasLove <ulrik.rosberg@outlook.com>
2022-09-01 11:04:53 +0200

    upload mvc project with Azure AD authentication

---
+++ AzureAD.Web/Controllers/AuthADController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAD.Web.Controllers
+{
+    public class AuthADController : Controller
+    {
+        public IActionResult SignInAD()
+        {
+            var scheme = OpenIdConnectDefaults.AuthenticationScheme;
+            var redirectUrl = Url.ActionContext.HttpContext.Request.Scheme 
+                + "://" + Url.ActionContext.HttpContext.Request.Host;
+            return Challenge(new AuthenticationProperties
+            {
+                RedirectUri = redirectUrl
+            }, scheme);
+        }
+
+        public IActionResult SignOutAD()
+        {
+            var scheme = OpenIdConnectDefaults.AuthenticationScheme;
+            return SignOut(new AuthenticationProperties(), 
+                CookieAuthenticationDefaults.AuthenticationScheme, scheme);
+        }
+    }
+}
